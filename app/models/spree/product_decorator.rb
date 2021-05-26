5a6ba12ac2fa01c6b5767699ba2e43377ca899c9
Emil Cabuhat <emilonrails@me.com>
2021-05-26 12:56:43 -0700

    update variant decorator, replacing class_eval with Module.prepend

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,3 +1,4 @@
+=begin
 Spree::Product.class_eval do
 
   # Essentially all read values here are delegated to reading the value on the Master variant
@@ -40,3 +41,61 @@ Spree::Product.class_eval do
     block.call master
   end
 end
+=end
+
+
+module Spree
+  module ProductDecorator
+    def self.prepended(base)
+      base.belongs_to :master
+      base.belongs_to :active_sale_in
+      base.belongs_to :current_sale_in
+      base.belongs_to :next_active_sale_in
+      base.belongs_to :next_current_sale_in
+      base.belongs_to :sale_price_in
+      base.belongs_to :on_sale_in?
+      base.belongs_to :original_price_in
+      base.belongs_to :discount_percent_in
+      base.belongs_to :sale_price
+      base.belongs_to :original_price
+    end
+    
+                      
+    #attr_accessible :sale_price, :original_price                    
+    # TODO Should the all_variants flag be on option you set on creating the sale and then it always behaves as such? Seems unsafe to pass this flag one way during create and use a different value for it later (they can actively bypass by accessing each variant directly and changing the values)
+
+    # TODO also accept a class reference for calculator type instead of only a string
+    def put_on_sale(value, calculator_type = "Spree::Calculator::DollarAmountSalePriceCalculator", all_variants = true, start_at = Time.now, end_at = nil, enabled = true)
+      run_on_variants(all_variants) { |v| v.put_on_sale(value, calculator_type, true, start_at, end_at, enabled) }
+    end
+    alias :create_sale :put_on_sale
+
+    def enable_sale(all_variants = true)
+      run_on_variants(all_variants) { |v| v.enable_sale }
+    end
+
+    def disable_sale(all_variants = true)
+      run_on_variants(all_variants) { |v| v.disable_sale }
+    end
+
+    def start_sale(end_time = nil, all_variants = true)
+      run_on_variants(all_variants) { |v| v.start_sale(end_time) }
+    end
+
+    def stop_sale(all_variants = true)
+      run_on_variants(all_variants) { |v| v.stop_sale }
+    end
+  
+    private 
+  
+    def run_on_variants(all_variants, &block)
+      if all_variants && variants.present?
+        variants.each { |v| block.call v } 
+      end
+      block.call master
+    end
+  end
+end
+
+
+::Spree::Variant.prepend(Spree::ProductDecorator)
